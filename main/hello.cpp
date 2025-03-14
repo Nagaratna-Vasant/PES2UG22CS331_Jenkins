4f6684beb92e1e6a9ceec2eaed75df58cf69c3d6
Nagaratna-Vasant <nagaratna0315@gmail.com>
2025-03-14 16:06:40 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, PES2UG22CS331\n";
   cout << "Hello, Nagaratna\n";
+  cout << "welcome to jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
